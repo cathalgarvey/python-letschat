0876bfaf5a2c938827650fa76b007876dcbd2a23
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2015-06-01 20:02:43 +0100

    Split and moved API class into a bare wrapper 'BaseAPI' and the higher-level/custom bits 'API'.

--- setup.py
+++ setup.py
@@ -22,10 +22,11 @@ setup(
     classifiers= [
       'Development Status :: 4 - Beta',
       'Intended Audience :: Developers',
-      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
       'Programming Language :: Python',
       'Programming Language :: Python :: 3.4',
       'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Communications :: Chat'
     ],
     long_description = open('ReadMe.md').read(),
     url='http://github.com/cathalgarvey/python-letschat'
